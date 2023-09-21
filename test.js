c44784dfd82bba1f0c939bdae3c523022805028e
raulmahya <raulmahya11@gmail.com>
2023-09-21 15:23:29 +0700

    push uni testing

--- test.js
+++ test.js
@@ -1,3 +1,50 @@
-function main () {
-  return asjdklbashjgdbvhjasdasdasdasdasdasdas
+const express = require('express')
+const mongoose = require('mongoose')
+const AccountModel = mongoose.model('Account')
+
+module.exports = () => {
+  const signUpRouter = new SignUpRouter() // Create an instance of SignUpRouter
+  const router = express.Router()
+
+  router.post('/signup', ExpressRouterAdapter.adapt(signUpRouter))
+
+  return router
+}
+
+class ExpressRouterAdapter {
+  static adapt (router) {
+    return async (req, res) => {
+      const httpRequest = {
+        body: req.body
+      }
+      const httpResponse = await router.route(httpRequest)
+      res.status(httpResponse.statusCode).json(httpResponse.body)
+    }
+  }
+}
+
+class SignUpRouter {
+  async route (httpRequest) {
+    const { email, password, repeatPassword } = httpRequest.body
+    const user = await new SignUpCase().signUp(email, password, repeatPassword)
+    return {
+      statusCode: 200,
+      body: user
+    }
+  }
+}
+
+class SignUpCase {
+  async signUp (email, password, repeatPassword) {
+    if (password === repeatPassword) {
+      return await new AddCountRepository().add(email, password)
+    }
+  }
+}
+
+class AddCountRepository {
+  async add (email, password) {
+    const user = await AccountModel.create({ email, password })
+    return user
+  }
 }
