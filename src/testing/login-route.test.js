9ec277c6129f06ea85c1e296202daa3c7e3bb475
raulmahya <raulmahya11@gmail.com>
2023-09-24 18:41:33 +0700

    push

--- src/testing/login-route.test.js
+++ src/testing/login-route.test.js
@@ -1,9 +1,13 @@
 const LoginRouter = require("./login-router");
 const MissingParamError = require("../helpers/missing-params"); // Import MissingParamError class
 
+const makeSut = () => {
+  return new LoginRouter();
+};
+
 describe("Login Router", () => {
   test("Should return 400 if no email is provided", async () => {
-    const sut = new LoginRouter();
+    const sut = makeSut();
     const httpRequest = {
       body: {
         password: "any_password",
@@ -16,7 +20,7 @@ describe("Login Router", () => {
   });
 
   test("Should return 400 if no password is provided", async () => {
-    const sut = new LoginRouter();
+    const sut = makeSut();
     const httpRequest = {
       body: {
         email: "any_password@mail.com",
@@ -29,14 +33,21 @@ describe("Login Router", () => {
   });
 
   test("Should return 500 if no httpRequest is provided", async () => {
-    const sut = new LoginRouter();
+    const sut = makeSut();
     const httpResponse = sut.route();
 
     expect(httpResponse.statusCode).toBe(500);
   });
 
   test("Should return 500 if httpRequest has no body", async () => {
-    const sut = new LoginRouter();
+    const sut = makeSut();
+    const httpResponse = sut.route({});
+
+    expect(httpResponse.statusCode).toBe(500);
+  });
+
+  test("Should call AuthUseCase with correct params", async () => {
+    const sut = makeSut();
     const httpResponse = sut.route({});
 
     expect(httpResponse.statusCode).toBe(500);
